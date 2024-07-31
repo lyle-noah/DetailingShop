47f9e1d58585e7cef26d1728e6990475e39c0ea8
ysw1208 <tony605a@naver.com>
2024-07-31 14:35:50 +0900

    login_form.html 생성 및 다시 삭제

--- src/main/java/com/green3rd/DetailingShop/user/UserController.java
+++ src/main/java/com/green3rd/DetailingShop/user/UserController.java
@@ -60,7 +60,7 @@ public class UserController {
 
     @GetMapping("/login")
     public String login() {
-        return "login_form";
+        return "forms/login_form";
     }
 
     @GetMapping("/profile")
