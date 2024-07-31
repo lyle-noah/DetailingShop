528cc3e5e391b5b4151816bfdf6083c16973f858
ysw1208 <tony605a@naver.com>
2024-07-31 12:49:12 +0900

    이전파일복원

---
+++ src/main/java/com/green3rd/DetailingShop/user/PasswordResetToken.java
@@ -0,0 +1,23 @@
+package com.green3rd.DetailingShop.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @ManyToOne
+    private SiteUser user;
+}
---
+++ src/main/java/com/green3rd/DetailingShop/user/PasswordResetTokenRepository.java
@@ -0,0 +1,7 @@
+package com.green3rd.DetailingShop.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+}
