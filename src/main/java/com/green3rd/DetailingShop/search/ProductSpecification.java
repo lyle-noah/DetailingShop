57458632aecf397fdbb8578aafca97226fb1e2cc
ysw1208 <tony605a@naver.com>
2024-08-06 15:57:04 +0900

    검색오류수정

--- src/main/java/com/green3rd/DetailingShop/search/ProductSpecification.java
+++ src/main/java/com/green3rd/DetailingShop/search/ProductSpecification.java
@@ -8,14 +8,14 @@ public class ProductSpecification {
 
 
     public static Specification<Product> hasProductName(String productName) {
-        return (root, query, cb) -> cb.like(root.get("Product_name"), "%" + productName + "%");
+        return (root, query, cb) -> cb.like(root.get("ProductName"), "%" + productName + "%");
     }
 
     public static Specification<Product> hasProductPrice(Integer productPrice) {
-        return (root, query, cb) -> cb.equal(root.get("Product_price"), productPrice);
+        return (root, query, cb) -> cb.equal(root.get("ProductPrice"), productPrice);
     }
 
     public static Specification<Product> hasRegistrationDate(LocalDateTime registrationDate) {
-        return (root, query, cb) -> cb.equal(root.get("Registration_date"), registrationDate);
+        return (root, query, cb) -> cb.equal(root.get("RegistrationDate"), registrationDate);
     }
 }
