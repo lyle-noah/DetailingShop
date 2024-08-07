8a54bfde10d0e1376fd55c7f6c08220787a5ab10
Lyle <st30309@naver.com>
2024-08-07 17:21:45 +0900

    20240807 금일 작업완료 - 각 카테고리별 상품목록 페이지 구축완료

--- src/main/java/com/green3rd/DetailingShop/ProductList/ProductService.java
+++ src/main/java/com/green3rd/DetailingShop/ProductList/ProductService.java
@@ -1,7 +1,11 @@
 package com.green3rd.DetailingShop.ProductList;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
 
 import java.text.NumberFormat;
 import java.util.List;
@@ -13,8 +17,15 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProductsByCategory(String category) {
-        return productRepository.findByFirstCategory(category);
+    public List<Product> getProductsByCategory(String firstCategory, String secondCategory, String thirdCategory) {
+        if (!secondCategory.isEmpty() && !thirdCategory.isEmpty()) {
+            return productRepository.findByFirstCategoryAndSecondCategoryAndThirdCategory(firstCategory, secondCategory, thirdCategory);
+        } else if (!secondCategory.isEmpty()) {
+            return productRepository.findByFirstCategoryAndSecondCategory(firstCategory, secondCategory);
+        } else if (!firstCategory.isEmpty()) {
+            return productRepository.findByFirstCategory(firstCategory);
+        }
+        return List.of();
     }
 
     /* 제품 가격타입(ex.2000) -> (ex.2,000원) 변경 변수*/
