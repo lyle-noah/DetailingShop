f82744bf2a2c748a8a2fb5515ef3e3d268e94593
Lyle <st30309@naver.com>
2024-08-13 08:58:41 +0900

    20240813 오전작업 - 간단작업

--- src/main/java/com/green3rd/DetailingShop/ProductList/ProductController.java
+++ src/main/java/com/green3rd/DetailingShop/ProductList/ProductController.java
@@ -7,12 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
