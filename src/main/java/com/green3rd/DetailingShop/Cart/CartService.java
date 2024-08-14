b337d30a24046f9f30a95847d59f4caa288a155a
ysw1208 <tony605a@naver.com>
2024-08-14 13:05:19 +0900

    장바구니 기능 구현 및 수정 카테고리폼 수정

--- src/main/java/com/green3rd/DetailingShop/Cart/CartService.java
+++ src/main/java/com/green3rd/DetailingShop/Cart/CartService.java
@@ -5,6 +5,7 @@ import com.green3rd.DetailingShop.ProductList.Product;
 import com.green3rd.DetailingShop.ProductList.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 
 import java.util.List;
 
@@ -35,14 +36,12 @@ public class CartService {
             cart = new Cart();
             // setUser 메서드를 사용해 SiteUser 설정
             cart.setUser(user);
+            cart.setProducts(new ArrayList<>());  // 새로 생성된 Cart에 빈 리스트로 초기화
         }
 
         // 장바구니 제품 목록 가져오기
         List<Product> products = cart.getProducts();
         products.add(product);
-        // 변경된 목록 설정
-        cart.setProducts(products);
-
         cartRepository.save(cart);
     }
 }
